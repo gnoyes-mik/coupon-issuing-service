f03b8c2db20fd8dc8c3bde79856d8a5cc5a40aad
jeff-seyong <sedragon.kim@gmail.com>
2024-03-21 16:43:11 +0900

    feature: rollback

--- coupon-api/src/main/java/me/gnoyes/couponapi/service/CouponIssueRequestService.java
+++ coupon-api/src/main/java/me/gnoyes/couponapi/service/CouponIssueRequestService.java
@@ -13,17 +13,11 @@ import org.springframework.stereotype.Service;
 public class CouponIssueRequestService {
 
     private final CouponIssueService couponIssueService;
-    private final DistributeLockExecutor distributeLockExecutor;
 
     private final Logger log = LoggerFactory.getLogger(this.getClass().getSimpleName());
 
     public void issueRequestV1(CouponIssueRequestDto requestDto) {
-        distributeLockExecutor.execute(
-                "lock_" + requestDto.couponId(),
-                10_000,
-                10_000,
-                () -> couponIssueService.issue(requestDto.couponId(), requestDto.userId())
-        );
+        couponIssueService.issue(requestDto.couponId(), requestDto.userId());
         log.info("쿠폰 발급 완료. couponId: %s, userId: %s".formatted(requestDto.couponId(), requestDto.userId()));
     }
 }
