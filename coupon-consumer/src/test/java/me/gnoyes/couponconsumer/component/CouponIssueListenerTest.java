eb2e4edb6b083f1993bd9fea0c84fd816d0ac6f8
jeff-seyong <sedragon.kim@gmail.com>
2024-03-23 21:03:34 +0900

    feature: add coupon issuing function

---
+++ coupon-consumer/src/test/java/me/gnoyes/couponconsumer/component/CouponIssueListenerTest.java
@@ -0,0 +1,88 @@
+package me.gnoyes.couponconsumer.component;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import me.gnoyes.couponconsumer.TestConfig;
+import me.gnoyes.couponcore.repository.redis.RedisRepository;
+import me.gnoyes.couponcore.service.CouponIssueService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InOrder;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+
+@Import(CouponIssueListener.class)
+class CouponIssueListenerTest extends TestConfig {
+
+    @Autowired
+    CouponIssueListener couponIssueListener;
+
+    @Autowired
+    RedisTemplate<String, String> redisTemplate;
+
+    @Autowired
+    RedisRepository redisRepository;
+
+    @MockBean
+    CouponIssueService couponIssueService;
+
+    @BeforeEach
+    void clear() {
+        Collection<String> redisKeys = redisTemplate.keys("*");
+        assert redisKeys != null;
+        redisTemplate.delete(redisKeys);
+    }
+
+    @Test
+    @DisplayName("쿠폰 발급 큐에 처리 대상이 없다면 발급을 하지 않는다")
+    void issue_1() throws JsonProcessingException {
+        // when
+        couponIssueListener.issue();
+        // then
+        verify(couponIssueService, never()).issue(anyLong(), anyLong());
+    }
+
+    @Test
+    @DisplayName("쿠폰 발급 큐에 처리 대상이 있다면 발급한다")
+    void issue_2() throws JsonProcessingException {
+        // given
+        long couponId = 1;
+        long userId = 1;
+        int totalQuantity = Integer.MAX_VALUE;
+        redisRepository.issueRequest(couponId, userId, totalQuantity);
+        // when
+        couponIssueListener.issue();
+        // then
+        verify(couponIssueService, times(1)).issue(anyLong(), anyLong());
+    }
+
+    @Test
+    @DisplayName("쿠폰 발급 요청 순서에 맞게 처리된다")
+    void issue_3() throws JsonProcessingException {
+        // given
+        long couponId = 1;
+        long userId1 = 1;
+        long userId2 = 2;
+        long userId3 = 3;
+        int totalQuantity = Integer.MAX_VALUE;
+        redisRepository.issueRequest(couponId, userId1, totalQuantity);
+        redisRepository.issueRequest(couponId, userId2, totalQuantity);
+        redisRepository.issueRequest(couponId, userId3, totalQuantity);
+        // when
+        couponIssueListener.issue();
+        // then
+        InOrder inOrder = Mockito.inOrder(couponIssueService);
+        inOrder.verify(couponIssueService, times(1)).issue(couponId, userId1);
+        inOrder.verify(couponIssueService, times(1)).issue(couponId, userId2);
+        inOrder.verify(couponIssueService, times(1)).issue(couponId, userId3);
+    }
+}
